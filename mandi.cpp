af302b2c630aa99a7732b69b78ab87d74a5f473a
Miftah RN <127042298+SerizeZR@users.noreply.github.com>
2023-08-29 01:01:03 +0700

    Update mandi.cpp

--- mandi.cpp
+++ mandi.cpp
@@ -3,39 +3,30 @@
 using namespace std;
 
 int main(){
-    // Langkah 1: Siapkan semua yang diperlukan
-    // Misalnya, handuk, sabun, sampo, air panas, dan baju ganti.
+    
+    bool adaAir = true;
 
-    // Langkah 2: Periksa apakah ada air panas
-    bool adaAirPanas = true;
-
-    if(adaAirPanas){
-        // Langkah 3: Masuki kamar mandi
+    if(adaAir){
         cout << "Masuk ke kamar mandi." << endl;
 
-        // Langkah 4: Buka keran air panas
-        cout << "Buka keran air panas." << endl;
+        cout << "Buka keran air." << endl;
 
-        // Langkah 5: Basahi tubuh dengan air panas
-        cout << "Basahi tubuh dengan air panas." << endl;
+        cout << "Basahi tubuh dengan air." << endl;
 
-        // Langkah 6: Gunakan sabun dan sampo
         cout << "Gunakan sabun dan sampo." << endl;
 
-        // Langkah 7: Bilas tubuh dengan air bersih
         cout << "Bilas tubuh dengan air bersih." << endl;
 
-        // Langkah 8: Matikan keran air panas
-        cout << "Matikan keran air panas." << endl;
+        cout << "Matikan keran air." << endl;
 
-        // Langkah 9: Keringkan diri dengan handuk
         cout << "Keringkan diri dengan handuk." << endl;
 
-        // Langkah 10: Keluar dari kamar mandi
         cout << "Keluar dari kamar mandi." << endl;
     }else{
-        cout << "Maaf, tidak ada air panas saat ini. Tunggu hingga air panas tersedia." << endl;
+        cout << "Maaf, tidak ada air saat ini. Tunggu hingga air tersedia." << endl;
     }
+
+    cin.get();
 
     return 0;
 }
